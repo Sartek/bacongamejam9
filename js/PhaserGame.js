bc7bd0599160695a387da2b1d34074ee943032c2
Andrew Richards <sartek.richards@gmail.com>
2015-06-13 20:47:17 +1000

    initial commit

---
+++ js/PhaserGame.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    'use strict';
+    var game,w,h;
+    w = 640;
+    h = 480;
+    game = new Phaser.Game(w, h, Phaser.AUTO, '', { preload: preload, create:       create, update: update });
+
+    function preload() {
+    }
+
+    function create() {
+    }
+
+    function update() {
+    }
+};
